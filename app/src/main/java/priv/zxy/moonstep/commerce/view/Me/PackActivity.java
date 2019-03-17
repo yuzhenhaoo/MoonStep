1ba1ac986158b1929739eaa69a08ef71c95f73fc
Joker <376358913@qq.com>
2019-03-18 00:09:28 +0800

    界面更新
    优化了背包界面的显示

--- app/src/main/java/priv/zxy/moonstep/commerce/view/Me/PackActivity.java
+++ app/src/main/java/priv/zxy/moonstep/commerce/view/Me/PackActivity.java
@@ -3,12 +3,17 @@ package priv.zxy.moonstep.commerce.view.Me;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.List;
+
 import priv.zxy.moonstep.R;
 import priv.zxy.moonstep.adapter.AbstractAdapter;
+import priv.zxy.moonstep.data.bean.BaseActivity;
 import priv.zxy.moonstep.framework.good.GoodInfoManager;
 import priv.zxy.moonstep.framework.good.bean.Good;
 
@@ -18,30 +23,26 @@ import priv.zxy.moonstep.framework.good.bean.Good;
  * 描述: 背包Activity
  **/
 
-public class PackActivity extends AppCompatActivity {
+public class PackActivity extends BaseActivity implements View.OnClickListener{
 
     private static final String TAG = "PackActivity";
     private GridView packView;
-    private LinearLayout fg;
+    private Button bt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pack);
-
-        initView();
-
-        initData();
-
-        initEvent();
+        setView(R.layout.activity_pack);
     }
 
-    private void initView(){
-        packView = findViewById(R.id.GridLayout);
-        fg = findViewById(R.id.fg_bottom);
+    @Override
+    protected void initEvent() {
+        bt.setOnClickListener(this);
     }
 
     public void initData(){
+        List<Good> goods = GoodInfoManager.getInstance().getGoods();
+        initGoods(goods);
         AbstractAdapter<Good> mAbstractAdapter = new AbstractAdapter<Good>(GoodInfoManager.getInstance().getGoods(), R.layout.pack_item) {
             @Override
             public void bindView(ViewHolder holder, Good obj) {
@@ -54,7 +55,24 @@ public class PackActivity extends AppCompatActivity {
         packView.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(PackActivity.this, "你点击了" + position + "个背包" , Toast.LENGTH_SHORT).show());
     }
 
-    public void initEvent(){
-        fg.setOnTouchListener((v, event) -> true);
+    private void initGoods(List<Good> goods) {
+        if (goods.size() < 4 * 6) {
+            for (int i = 4 * 6 - goods.size(); i > 0; i--) {
+                goods.add(new Good());
+            }
+        }
+    }
+
+    @Override
+    protected void initView() {
+        packView = findViewById(R.id.gridView);
+        bt = findViewById(R.id.back);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(bt)) {
+            finish();
+        }
     }
 }
--- app/src/main/java/priv/zxy/moonstep/commerce/view/Me/RaceActivity.java
+++ app/src/main/java/priv/zxy/moonstep/commerce/view/Me/RaceActivity.java
@@ -3,9 +3,9 @@ package priv.zxy.moonstep.commerce.view.Me;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import priv.zxy.moonstep.R;
@@ -14,23 +14,17 @@ import priv.zxy.moonstep.data.bean.BaseActivity;
 import priv.zxy.moonstep.framework.race.Race;
 
 /**
- * 创建人: Administrator
- * 创建时间: 2018/11/9
- * 描述: 用来展示用户详情页面的Activity
+ * 创建人: LYJ
+ * 创建时间: 2019/02/09
+ * 描述: 用来展示用户种族信息的页面
  **/
 
-public class RaceActivity extends BaseActivity implements IUserDetailView{
+public class RaceActivity extends BaseActivity implements IUserDetailView, View.OnClickListener{
 
     private UserDetailPresenter mUserDetailPresenter;
 
     private Button back;
-
-    private TextView raceNameTV;
-
-    private TextView raceDescriptionTV;
-
     private ImageView raceHeadPortrait;
-
     private ImageView raceIcon;
 
     @Override
@@ -40,28 +34,22 @@ public class RaceActivity extends BaseActivity implements IUserDetailView{
     }
 
     protected void initView(){
-        mUserDetailPresenter = new UserDetailPresenter(this);
-        back = findViewById(R.id.backBt);
-        raceNameTV = findViewById(R.id.raceNameTV);
-        raceDescriptionTV = findViewById(R.id.raceDescriptionTV);
+        back = findViewById(R.id.back);
         raceHeadPortrait = findViewById(R.id.raceHeadPortrait);
         raceIcon = findViewById(R.id.raceIcon);
     }
 
     protected void initData(){
+        mUserDetailPresenter = new UserDetailPresenter(this);
         mUserDetailPresenter.setRaceData();
     }
 
     protected void initEvent(){
-        back.setOnClickListener(v->{
-            RaceActivity.this.finish();
-        });
+        back.setOnClickListener(this);
     }
 
     @Override
     public void initRaceData(Race race){
-        raceNameTV.setText(race.getRaceName());
-        raceDescriptionTV.setText(race.getRaceDescription());
     }
 
     @Override
@@ -77,5 +65,12 @@ public class RaceActivity extends BaseActivity implements IUserDetailView{
     @Override
     public void error(String error_msg){
         Toast.makeText(this, error_msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(back)) {
+            finish();
+        }
     }
 }
